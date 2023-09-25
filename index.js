8b56cf2e9d05ee6bd9eb69b1ad522e1dbd9ade56
SammyRuheni <marvinmuthama03@gmail>
2023-09-25 16:13:18 +0300

    Completed the assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+// Addition
+function add(a, b) {
+    return a + b;
+  }
+  
+  // Subtraction
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  // Multiplication
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  // Division
+  function divide(a, b) {
+    return a / b;
+  }
+  
+// Increment by 1
+function increment(n) {
+    return ++n;
+  }
+  
+  // Decrement by 1
+  function decrement(n) {
+    return --n;
+  }
+
+// Parse a string to an integer with base 10
+function makeInt(string) {
+    return parseInt(string, 10);
+  }
+  
+  // Parse a string to a floating-point number
+  function preserveDecimal(string) {
+    return parseFloat(string);
+  }
+  
